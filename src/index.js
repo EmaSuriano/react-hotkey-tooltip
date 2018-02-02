fcb77abd81a66a89047bd0190ae1bf75e562f031
EmaSuriano <emanuel.suriano@gmail.com>
2018-02-02 15:50:33 -0300

    breaking build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 export default class extends Component {
   render() {
-    return <div>
+    return 
       <h2>Welcome to React components</h2>
     </div>
   }
