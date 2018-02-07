ae968ccea516381e2d9f4ef14df75da0b3e43365
EmaSuriano <emanuel.suriano@gmail.com>
2018-02-07 17:58:24 -0300

    test(helpTooltipHandler): adding test if showTooltipFunc is being called after keydown or keyup
    BREAKING CHANGE: new api

--- __tests__/helpTooltipHandler.js
+++ __tests__/helpTooltipHandler.js
@@ -10,7 +10,12 @@ jest.mock('mousetrap', () => ({
 }));
 
 describe('helpTooltipHandler', () => {
+  let callHandlersKeydown;
+  let callHandlersKeyup;
+
   describe('addNewTooltipHelp', () => {
+    let callHandlers;
+
     beforeEach(() => {
       MouseTrap.bind.mockClear();
     });
@@ -19,9 +24,11 @@ describe('helpTooltipHandler', () => {
       const showTooltipSpy = jest.fn();
       addNewTooltipHelp('a', showTooltipSpy);
 
-      expect(MouseTrap.bind.mock.calls.length).toBe(2);
-
       const [firstCall, secondCall] = MouseTrap.bind.mock.calls;
+      callHandlersKeydown = firstCall[1];
+      callHandlersKeyup = secondCall[1];
+
+      expect(MouseTrap.bind.mock.calls.length).toBe(2);
       expect(firstCall[0]).toBe('a');
       expect(firstCall[2]).toBe('keydown');
 
@@ -35,11 +42,45 @@ describe('helpTooltipHandler', () => {
 
       expect(MouseTrap.bind).not.toHaveBeenCalled();
     });
+
+    it('should call each function passed after keyup or keydown', () => {
+      const showTooltipSpy1 = jest.fn();
+      const showTooltipSpy2 = jest.fn();
+
+      addNewTooltipHelp('a', showTooltipSpy1);
+      addNewTooltipHelp('a', showTooltipSpy2);
+
+      callHandlersKeydown();
+      callHandlersKeyup();
+      expect(showTooltipSpy1).toHaveBeenCalledWith(true);
+      expect(showTooltipSpy1).toHaveBeenCalledWith(false);
+
+      expect(showTooltipSpy2).toHaveBeenCalledWith(true);
+      expect(showTooltipSpy2).toHaveBeenCalledWith(false);
+    });
   });
 
   describe('removeTooltipHelp', () => {
-    it('should remove showTooltip function from hotkeysTooltip if it exists', () => {});
+    it('should remove showTooltip function from hotkeysTooltip if it exists', () => {
+      const showTooltipSpy1 = jest.fn();
 
-    it('should not change hotkeysTooltip when showTooltip function does not exists in it', () => {});
+      addNewTooltipHelp('a', showTooltipSpy1);
+      removeToolTipHelp('a', showTooltipSpy1);
+
+      callHandlersKeydown();
+
+      expect(showTooltipSpy1).not.toHaveBeenCalled();
+    });
+
+    it('should not change hotkeysTooltip when showTooltip function does not exists in it', () => {
+      const showTooltipSpy1 = jest.fn();
+
+      addNewTooltipHelp('a', showTooltipSpy1);
+      removeToolTipHelp('a', jest.fn());
+
+      callHandlersKeydown();
+
+      expect(showTooltipSpy1).toHaveBeenCalled();
+    });
   });
 });
