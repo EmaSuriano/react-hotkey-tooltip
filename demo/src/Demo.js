76de491e55c8a8038f77b93c22a19ac90be909ce
EmaSuriano <emanuel.suriano@gmail.com>
2018-02-08 10:44:25 -0300

    revert(Demo): Changing demo structure
    BREAKING CHANGE: just to improve number in semantic-release

--- demo/src/Demo.js
+++ demo/src/Demo.js
@@ -3,17 +3,22 @@ import { render } from 'react-dom';
 import Menu from './Menu1';
 import './Demo.css';
 
-class Demo extends Component {
+export default class Demo extends Component {
+  state = {
+    showMenu: true,
+  };
+
+  toggleMenu = () => this.setState({ showMenu: !this.state.showMenu });
+
   render() {
     return (
       <div className="main-container">
         <h1>react-hotkey-tooltip</h1>
         <div className="container">
-          <Menu />
+          <button onClick={this.toggleMenu}>Toggle</button>
+          {this.state.showMenu && <Menu />}
         </div>
       </div>
     );
   }
 }
-
-export default Demo;
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,27 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-// import Demo from './Demo';
-import Menu from './Menu1';
-import './Demo.css';
-
-class Demo extends Component {
-  state = {
-    showMenu: true,
-  };
-
-  toggleMenu = () => this.setState({ showMenu: !this.state.showMenu });
-
-  render() {
-    return (
-      <div className="main-container">
-        <h1>react-hotkey-tooltip</h1>
-        <div className="container">
-          <button onClick={this.toggleMenu}>Toggle</button>
-          {this.state.showMenu && <Menu />}
-        </div>
-      </div>
-    );
-  }
-}
+import Demo from './Demo';
 
 render(<Demo />, document.querySelector('#demo'));
