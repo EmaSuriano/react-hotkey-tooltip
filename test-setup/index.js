8bfdc5cf7d616ceb16a008322439fa6557123542
EmaSuriano <emanuel.suriano@gmail.com>
2018-07-24 20:31:43 -0300

    chore(package.json): remove unused jest config

--- test-setup/index.js
+++ test-setup/index.js
@@ -2,9 +2,4 @@ import Enzyme from 'enzyme';
 // import Adapter from 'enzyme-adapter-react-16';
 import Adapter from './ReactSixteenAdapter'; // FIX: when enzyme supports for context tag, replace this with enzyme-adapter-react-16
 
-// jest.mock('mousetrap', () => ({
-//   bind: jest.fn(),
-//   unbind: jest.fn(),
-// }));
-
 Enzyme.configure({ adapter: new Adapter() });
