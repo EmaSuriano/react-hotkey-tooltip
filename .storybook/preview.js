2abbb3b01d5b3df3e2867999b8ad1b00ed514d4a
EmaSuriano <emanuel.suriano@gmail.com>
2020-03-30 23:17:48 +0200

    clean up deps

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import { Typography } from '@material-ui/core';
 import 'typeface-roboto';
 import 'tippy.js/dist/tippy.css';
@@ -11,17 +10,8 @@ addParameters({
   options: {
     showRoots: true,
   },
-  docs: {
-    container: DocsContainer,
-    page: DocsPage,
-  },
   dependencies: {
-    //display only dependencies/dependents that have a story in storybook
-    //by default this is false
     withStoriesOnly: true,
-
-    //completely hide a dependency/dependents block if it has no elements
-    //by default this is false
     hideEmpty: true,
   },
 });
