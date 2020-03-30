d26b9ef2eaeda5a13831c0b7eaea28865d551868
EmaSuriano <emanuel.suriano@gmail.com>
2020-03-30 23:05:30 +0200

    clean up old code

--- old/index.js
+++
@@ -1,5 +0,0 @@
-import Hotkey from './Hotkey';
-import HotkeyProvider from './HotkeyProvider';
-import 'react-tippy/dist/tippy.css';
-
-export { HotkeyProvider, Hotkey };
--- old/utils copy.js
+++
@@ -1,7 +0,0 @@
-export const isFunction = v => typeof v === 'function';
-
-export const KEYBOARD_EVENT = {
-  KEY_PRESS: 'keypress',
-  KEY_DOWN: 'keydown',
-  KEY_UP: 'keyup',
-};
